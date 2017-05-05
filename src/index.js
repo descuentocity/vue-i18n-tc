0347339c863fa7dff13c1bfa35040e71b9c5f48b
Nicolás Bevilacqua <nicobevilacqua@gmail.com>
2017-05-05 11:34:06 -0300

    cambios para ssr

--- src/index.js
+++ src/index.js
@@ -1,56 +1,76 @@
 import localize from './lib/localize';
 import ajax from './lib/ajax';
 
-class LocalesLoader {
-  constructor({ lang, SSR, path, get, locales }) {
-    this.locales = {};
-    this.observers = {};
-    if (locales) {
-      this.locales = locales;
-      return;
-    }
+function getLocales({ SSR, path, get }) {
+  return new Promise((resolve) => {
     if (get) {
       get().then((json) => {
-        this.locales[lang] = json;
-        this.observers[lang].map(observer => observer(json));
+        resolve(json);
       }).catch((error) => {
         console.error(error);
+        resolve({});
       });
       return;
     }
     if (SSR) {
       const json = require(path);
-      this.locales[lang] = json;
-      this.observers[lang].map(observer => observer(json));
+      resolve(json);
       return;
     }
     ajax({ path }).promise.then((json) => {
-      this.locales[lang] = json;
-      this.observers[lang].map(observer => observer(json));
+      resolve(json);
+    }).catch((error) => {
+      console.error(error);
+      resolve({});
+    });
+  });
+}
+
+class LocalesLoader {
+  constructor({ SSR, path, get, locales }) {
+    this.locales = undefined;
+    this.observers = [];
+    getLocales({ SSR, path, get, locales }).then((localesJson) => {
+      this.locales = localesJson;
+      this.observers.map(observer => observer(localesJson));
     }).catch((error) => {
       console.error(error);
     });
   }
-  subscribe(lang, observer) {
-    if (this.locales[lang]) {
-      observer(this.locales[lang]);
+  subscribe(observer) {
+    if (this.locales) {
+      observer(this.locales);
       return;
     }
-    this.observers[lang] = (this.observers[lang] || []);
-    this.observers[lang].push(observer);
+    this.observers = (this.observers || []);
+    this.observers.push(observer);
   }
 }
 
 export default {
   install: (Vue, { lang, SSR, path, get, locales }) => {
-    const localesLoader = new LocalesLoader({ lang, SSR, path, get, locales });
+    if ((SSR && locales) || locales) {
+      Vue.mixin({
+        data: () => ({
+          lang,
+          locales,
+        }),
+        methods: {
+          __(...args) {
+            return localize(this.locales, ...args);
+          },
+        },
+      });
+      return;
+    }
+    const localesLoader = new LocalesLoader({ lang, SSR, path, get });
     Vue.mixin({
       data: () => ({
         lang,
         locales: {},
       }),
       created() {
-        localesLoader.subscribe(lang, (localesJson) => {
+        localesLoader.subscribe((localesJson) => {
           this.locales = localesJson;
         });
       },
